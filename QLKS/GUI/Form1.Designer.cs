cee46f424ea771b26582342f6dfb84901b2b20fd
Huyhenz <tudomb2004@gmail.com>
2024-12-19 23:11:25 +0700

    Da cap nhat form dang nhap

--- QLKS/GUI/Form1.Designer.cs
+++ QLKS/GUI/Form1.Designer.cs
@@ -28,50 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.txt_User = new System.Windows.Forms.TextBox();
             this.txt_Pass = new System.Windows.Forms.TextBox();
             this.btn_Login = new DevExpress.XtraEditors.SimpleButton();
             this.btn_Exit = new DevExpress.XtraEditors.SimpleButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(181, 45);
-            this.labelControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(407, 106);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(4);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(67, 16);
+            this.labelControl1.Size = new System.Drawing.Size(103, 24);
             this.labelControl1.TabIndex = 0;
             this.labelControl1.Text = "Username: ";
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(181, 111);
-            this.labelControl2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(407, 176);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(4);
             this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(64, 16);
+            this.labelControl2.Size = new System.Drawing.Size(97, 24);
             this.labelControl2.TabIndex = 1;
             this.labelControl2.Text = "Password: ";
             // 
             // txt_User
             // 
-            this.txt_User.Location = new System.Drawing.Point(252, 42);
+            this.txt_User.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_User.Location = new System.Drawing.Point(517, 103);
             this.txt_User.Name = "txt_User";
-            this.txt_User.Size = new System.Drawing.Size(100, 23);
+            this.txt_User.Size = new System.Drawing.Size(244, 32);
             this.txt_User.TabIndex = 2;
             // 
             // txt_Pass
             // 
-            this.txt_Pass.Location = new System.Drawing.Point(252, 108);
+            this.txt_Pass.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Pass.Location = new System.Drawing.Point(517, 176);
             this.txt_Pass.Name = "txt_Pass";
-            this.txt_Pass.Size = new System.Drawing.Size(100, 23);
+            this.txt_Pass.Size = new System.Drawing.Size(244, 32);
             this.txt_Pass.TabIndex = 3;
             // 
             // btn_Login
             // 
-            this.btn_Login.Location = new System.Drawing.Point(158, 191);
-            this.btn_Login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Login.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.Appearance.Options.UseFont = true;
+            this.btn_Login.Location = new System.Drawing.Point(429, 267);
+            this.btn_Login.Margin = new System.Windows.Forms.Padding(4);
             this.btn_Login.Name = "btn_Login";
             this.btn_Login.Size = new System.Drawing.Size(118, 36);
             this.btn_Login.TabIndex = 4;
@@ -80,19 +91,33 @@
             // 
             // btn_Exit
             // 
-            this.btn_Exit.Location = new System.Drawing.Point(391, 191);
-            this.btn_Exit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Exit.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Exit.Appearance.Options.UseFont = true;
+            this.btn_Exit.Location = new System.Drawing.Point(619, 267);
+            this.btn_Exit.Margin = new System.Windows.Forms.Padding(4);
             this.btn_Exit.Name = "btn_Exit";
             this.btn_Exit.Size = new System.Drawing.Size(118, 36);
             this.btn_Exit.TabIndex = 5;
             this.btn_Exit.Text = "Exit";
             this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(388, 372);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Options.UseBackColor = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(627, 352);
+            this.ClientSize = new System.Drawing.Size(806, 438);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btn_Exit);
             this.Controls.Add(this.btn_Login);
             this.Controls.Add(this.txt_Pass);
@@ -103,6 +128,7 @@
             this.Name = "Form1";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,6 +142,7 @@
         private System.Windows.Forms.TextBox txt_Pass;
         private DevExpress.XtraEditors.SimpleButton btn_Login;
         private DevExpress.XtraEditors.SimpleButton btn_Exit;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
